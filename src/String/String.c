9ef875a27a5503e36443f6f895eccd30ceca7f72
Philip Knudsen <philipalexkndusen@gmail.com>
2023-02-03 10:09:24 +0100

    Started impl of String.c

--- src/String/String.c
+++ src/String/String.c
@@ -5,22 +5,46 @@
 #include "./String.h"
 
 String String_create_string() {
-  String result = {0};
+  char buffer[DEFAULT_STRING_SIZE];
+  String result = {
+    .chars = buffer,
+    .length = 0,
+    .capacity = DEFAULT_STRING_SIZE
+  };
   return result;
 }
 
 int String_add_char(String *dest, char c) {
-  UNIMPLIMENTED;
+  if (dest->length+1 > dest->capacity)
+    String_extend_string_size(dest, dest->capacity+1);
+  strcat(dest->chars, &c);
+  dest->length++;
+  return 1;
 }
 
 int String_add_cstr(String *dest, char *s) {
-  UNIMPLIMENTED;
+  size_t source_len = strlen(s);
+  if (dest->length+source_len > dest->capacity)
+    String_extend_string_size(dest, dest->capacity+source_len+1);
+  strcat(dest->chars, s);
+  dest->length += source_len;
+  return 1;
 }
 
-char *String_pop_char(String *target) {
+int String_pop_char(String *target, char *dest) {
   UNIMPLIMENTED;
 }
 
 static int String_extend_string_size(String *target, size_t new_size) {
-  UNIMPLIMENTED;
+  char string[new_size];
+  size_t final_size;
+  for (size_t i = 0; i < new_size; ++i) {
+    if (new_size > target->length) {
+      final_size = i;
+      break;
+    }
+    string[i] = target->chars[i];
+  }
+  target->length = final_size;
+  return 1;
 }
--- src/String/String.h
+++ src/String/String.h
@@ -1,17 +1,20 @@
+#include <stdlib.h>
 
 #ifndef STRING_H_
 #define STRING_H_
 
+#define DEFAULT_STRING_SIZE 32
+
 typedef struct {
   char *chars;
-  size_t size;
+  size_t length;
   size_t capacity;
 } String;
 
 String String_create_string();
 int String_add_char(String *dest, char c);
 int String_add_cstr(String *dest, char *s);
-char *String_pop_char(String *target);
+int String_pop_char(String *target, char *dest);
 static int String_extend_string_size(String *target, size_t new_size);
 
 #endif // STRING_H
